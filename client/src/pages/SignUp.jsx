bac11e0a66d451a1ed692e85225b11dbf1058984
Faisal Ahmed <a.faisal23r@gmail.com>
2024-01-13 19:42:30 +0600

    Complete SignUp page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,33 +1,96 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  // Show or Hide password
+
+  const passwordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  // Submit Form
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+
+      const res = await fetch("/api/auth/sign-up", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
-    <div className="px-3 max-w-6xl mx-auto">
+    <div className="px-3 sm:px-0 max-w-3xl mx-auto">
       <h1 className="text-3xl text-center text-semibold py-7">Sign up</h1>
-      <form className="flex flex-col gap-4 ">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
         <input
+          onChange={handleChange}
           type="text"
           className="border p-3 rounded-lg"
           id="username"
           placeholder="username"
         />
         <input
+          onChange={handleChange}
           type="text"
           className="border p-3 rounded-lg"
           id="email"
           placeholder="email"
         />
-        <input
-          type="text"
-          className="border p-3 rounded-lg"
-          id="password"
-          placeholder="password"
-        />
+
+        <div className="w-full relative ">
+          <input
+            onChange={handleChange}
+            type={showPassword ? "text" : "password"}
+            className="border p-3 rounded-lg w-full"
+            id="password"
+            placeholder="password"
+          />
+          <div
+            className="absolute top-4 right-2 sm:right-4 text-xl"
+            onClick={passwordVisibility}
+          >
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </div>
+        </div>
+
         <button
+          disabled={loading}
           className="bg-slate-700 py-3 rounded-lg text-white uppercase  text-xl hover:opacity-90 disabled:opacity-80 transition duration-200 ease-in-out "
           type="submit"
         >
-          Sing Up
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex items-center gap-2 mt-4">
@@ -36,6 +99,7 @@ const SignUp = () => {
           Sign in
         </Link>
       </div>
+      {error && <p className="text-2xl text-red-600 mt-5">{error}</p>}
     </div>
   );
 };
